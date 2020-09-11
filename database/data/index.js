1ff0059aacad41090f60bf596c994efb5943e093
Angel Zacarias <luis.zacariasm@alumnos.udg.mx>
2020-09-10 20:27:39 -0500

    new configuration deleted unnnecesary files

--- database/data/index.js
+++
@@ -1,5 +0,0 @@
-const areas = require('./areas.js');
-const questionStrings = require('./questions.js');
-const areaQuestionRelationships = require('./area-question-relationship');
-
-module.exports = { areas, questionStrings, areaQuestionRelationships };
